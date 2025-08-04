5677bd57bf64144b5bf954ae277f0a0ac5dcfcf1
dovewith999 <limhs991013@gmail.com>
2025-08-04 15:48:09 +0900

    NEW : Tetris Block Actor 생성
    - 테트리스 블럭으로 이용할 액터 클래스 생성

---
+++ Client/Client/Actor/TetrisBlock.cpp
@@ -0,0 +1,24 @@
+﻿#include "TetrisBlock.h"
+
+TetrisBlock::TetrisBlock()
+{
+}
+
+TetrisBlock::~TetrisBlock()
+{
+}
+
+void TetrisBlock::BeginPlay()
+{
+	super::BeginPlay();
+}
+
+void TetrisBlock::Tick(float deltaTime)
+{
+	super::Tick(deltaTime);
+}
+
+void TetrisBlock::Render()
+{
+	super::Render();
+}
---
+++ Client/Client/Actor/TetrisBlock.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Actor/Actor.h"
+
+class TetrisBlock final : public Actor
+{
+	RTTI_DECLARATIONS(TetrisBlock, Actor)
+
+public:
+	explicit TetrisBlock();
+	virtual ~TetrisBlock();
+
+public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float deltaTime) override;
+	virtual void Render() override;
+};
+
