8494bd6ab56dce6658f4722a8a55cae91891fef4
dovewith999 <limhs991013@gmail.com>
2025-08-04 17:39:07 +0900

    NEW : TetrisMultiLevel 생성
    - 멀티 환경에서의 Level 생성

---
+++ Client/Client/Level/TetrisMultiLevel.cpp
@@ -0,0 +1,30 @@
+﻿#include "TetrisMultiLevel.h"
+#include "Managers/FileManager.h"
+#include "Managers/SoundManager.h"
+#include "Managers/NetworkManager.h"
+
+void TetrisMultiLevel::BeginPlay()
+{
+	super::BeginPlay();
+}
+
+void TetrisMultiLevel::Tick(float deltaTime)
+{
+	super::Tick(deltaTime);
+
+	if (calledBeginPlay == true)
+	{
+		return;
+	}
+
+	calledBeginPlay = true;
+	system("cls");
+	//SoundManager::GetInstance()->PlaySoundW(L"BGM Tetris_Nintendo music.mp3", Define::ESoundChannelID::BGM, 10.f);
+
+	FileManager::GetInstance()->ReadFile("Map", Define::EFileType::MAP);
+}
+
+void TetrisMultiLevel::Render()
+{
+	super::Render();
+}
---
+++ Client/Client/Level/TetrisMultiLevel.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Level/Level.h"
+
+class TetrisMultiLevel final : public Level
+{
+	RTTI_DECLARATIONS(TetrisMultiLevel, Level)
+
+public:
+	explicit TetrisMultiLevel() = default;
+	virtual ~TetrisMultiLevel() = default;
+
+public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float deltaTime) override;
+	virtual void Render() override;
+
+private:
+	bool isGamePaused = false;
+	bool isGameOver = false;
+
+	float gameTimer = 0.f;
+
+};
+
